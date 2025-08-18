eac383a14380caf0326f2a24289c7b143e9cb692
Teti K <keshinrot@gmail.com>
2025-08-18 12:57:24 +0100

    Updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -98,7 +98,7 @@ function Navbar() {
 
   return (
     <nav
-      className={`fixed top-0 left-0 w-full z-50 transition-all duration-300 py-6 ${
+      className={`fixed top-0 left-0 w-full z-50 transition-all duration-300 py-16 ${
         scrolled ? 'bg-white/60 backdrop-blur-md shadow-md' : 'bg-transparent'
       }`}
     >
